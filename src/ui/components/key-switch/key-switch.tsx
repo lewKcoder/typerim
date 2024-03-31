ca5057dd0c1541a2947953a1ec369fab3fc24453
lewKcoder <7kamimura74@gmail.com>
2024-03-31 13:27:33 +0900

    feat: :sparkles: Added sound and use-sound

--- src/ui/components/key-switch/key-switch.tsx
+++ src/ui/components/key-switch/key-switch.tsx
@@ -1,16 +1,25 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import useSound from "use-sound";
+import catSound from "@/assets/cat.mp3";
+import { useEffect, useState, useRef } from "react";
 import { Component } from "./types";
 
 export const KeySwitch: Component = (props) => {
   const { label, isPressed: propIsPressed } = props;
   const [isPressed, setIsPressed] = useState(propIsPressed);
+  const [play] = useSound(catSound);
+
+  const isPressedRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const handleKeyDown = (event: KeyboardEvent) => {
       if (event.key === label || event.key === label.toLowerCase()) {
         setIsPressed(true);
+
+        if (isPressedRef.current) {
+          isPressedRef.current.click();
+        }
       }
     };
 
@@ -28,7 +37,14 @@ export const KeySwitch: Component = (props) => {
   }, []);
 
   return (
-    <button className="w-14 h-14 bg-black rounded">
+    <button
+      className="w-14 h-14 bg-black rounded"
+      ref={isPressedRef}
+      onClick={(event) => {
+        event.preventDefault();
+        play();
+      }}
+    >
       <div
         className={`w-[3.375rem] h-[3.321rem] leading-[3.321rem] text-[1.25rem] bg-[#2B2B2B] rounded translate-x-[1px] text-white font-bold ${
           isPressed ? "translate-y-[1px]" : "translate-y-[-1px]"
