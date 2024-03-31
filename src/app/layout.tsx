bb4d77a549cb74e1018eea3eb25a4ead866383ff
lewKcoder <7kamimura74@gmail.com>
2024-03-31 14:53:49 +0900

    fix: :bug: Fixed OGP

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,12 +18,12 @@ export default function RootLayout({
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="/OGP.png" />
-        <meta name="twitter:card" content="/OGP.png" />
+        <meta property="og:image" content="./OGP.png" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:site" content="@lew_yumei" />
         <meta name="twitter:title" content={metadata.title} />
         <meta name="twitter:description" content={metadata.description} />
-        <meta name="twitter:image" content="/OGP.png" />
+        <meta name="twitter:image" content="./OGP.png" />
       </head>
 
       <body className="bg-neutral-800">{children}</body>
